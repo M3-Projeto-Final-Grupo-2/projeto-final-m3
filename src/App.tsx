4dbf3e91018b3c4b839e0b0cd067499288a6e1d6
Willian Mascarenhas <masca.will@gmail.com>
2023-03-06 16:08:10 -0300

    feat: router in app/BrowserRouter in main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Router } from './routes'
 import { GlobalStyles } from './styles/global'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
      <GlobalStyles/>
+     <Router/>
     </div>
   )
 }
