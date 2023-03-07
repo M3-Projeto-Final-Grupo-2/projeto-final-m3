e6473d087af2b935b2d764c9f255da3563ada5f8
Willian Mascarenhas <masca.will@gmail.com>
2023-03-06 22:08:32 -0300

    feat: open/close modalEdit, render cards profilePage

--- src/providers/profileContext.tsx
+++ src/providers/profileContext.tsx
@@ -1,48 +1,92 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 import { toast } from 'react-toastify';
+import { useNavigate } from "react-router";
 
 
-export const ProfileContext = createContext({});
+export const ProfileContext = createContext({} as IProfileContext);
+
+interface ICities {
+    userId: number
+    name: string
+    state: string
+    description: string
+    image: string
+    id: number
+    data?: string 
+}
 
 interface IProviderProps {
   children: ReactNode;
 }
 
+interface IData {
+    description: string,
+    image: string
+}
+
+interface IProfileContext {
+    editCityFromUser: (data: IData) => Promise<void>
+    cities: ICities[] | Array<null>
+    logoutUser: () => void
+    homePageUser: () => void
+    modal: boolean
+    setModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
 const userId = 2
 const cityId = 2
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RlQG1haWwuY29tIiwiaWF0IjoxNjc4MTMyMzQ2LCJleHAiOjE2NzgxMzU5NDYsInN1YiI6IjIifQ.W9dM5ag9T5Q9bSZxxFJ3XB8M3QYkh8mh9khZnDNz3Qw"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RlQG1haWwuY29tIiwiaWF0IjoxNjc4MTQ4OTMwLCJleHAiOjE2NzgxNTI1MzAsInN1YiI6IjIifQ.JdS8GALWtRb2aBFhLEMbZc2f81wk24kzIAaEpDloxdo"
 
 export const ProfileProvider = ({children}: IProviderProps) => {
 
-    const getAllCitiesFromUser = async ()=>{
-        try {
-            const response = await api.get(`/cities?userid=${userId}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                }
-            })
-            console.log(response.data)
-        } catch (error) {
-            toast.error("Error 404, por favor tente novamente")
-        }
-    }
+    const [cities, setCities] = useState<ICities[] | null[]>([])
 
-    const editCityFromUser = async () =>{
+    useEffect(() =>{
+        const getAllCitiesFromUser = async ()=>{
+            // const token = localStorage.getItem("")
+            try {
+                const response = await api.get(`/cities?userid=${userId}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    }
+                })
+                setCities(response.data)
+            } catch (error) {
+                toast.error("Error 404, por favor tente novamente")
+            }
+        }
+        getAllCitiesFromUser()
+    }, [])
+
+    const editCityFromUser = async (data: IData) =>{
+            // const token = localStorage.getItem("")
         try {
-            const reponse = await api.patch(`/cities/${cityId}`, {
+            const reponse = await api.patch(`/cities/${cityId}`,  data, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 }
             })
-            console.log(reponse)
         } catch (error) {
             toast.error("Error, por favor tente novamente")
         }
     }
 
+    const Navigate = useNavigate()
+
+    const logoutUser = () =>{
+        // localStorage.removeItem("")
+        // Navigate("/")
+    }
+    const homePageUser = () =>{
+        // Navigate("/home")
+
+    }
+
+    const [modal, setModal] = useState(false)
+
     
-  return <ProfileContext.Provider value={{}}>
+  return <ProfileContext.Provider value={{cities, editCityFromUser, logoutUser, homePageUser, modal, setModal}}>
     {children}
     </ProfileContext.Provider>;
 };
