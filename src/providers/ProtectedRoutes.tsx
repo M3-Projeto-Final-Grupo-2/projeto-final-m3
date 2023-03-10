ab990cef57e65535eb6d354c19cb1425e78ad9f3
Willian Mascarenhas <masca.will@gmail.com>
2023-03-09 21:11:48 -0300

    feat: autoLogin/ protectedRoutes

---
+++ src/providers/ProtectedRoutes.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react"
+import { Navigate, Outlet } from "react-router-dom"
+import { LoginContext } from "./loginContext"
+
+interface IRoute {
+  ProtectedRoutes: ()  => JSX.Element
+}
+
+
+export const ProtectedRoutes = ()  =>{
+
+  const { user } = useContext(LoginContext)
+
+  return user? <Outlet/> : <Navigate to='/'/>
+} 
