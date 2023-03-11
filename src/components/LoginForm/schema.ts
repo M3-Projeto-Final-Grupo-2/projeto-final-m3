f24b60e19737d590a533b615128b2413369a7922
Elias Jr <eliasbarroso222@gmail.com>
2023-03-11 18:20:55 -0300

    fix: fixed styles login form

--- src/components/LoginForm/schema.ts
+++ src/components/LoginForm/schema.ts
@@ -1,14 +1,10 @@
 import * as yup from "yup";
 
-const Schema = yup
-  .object()
-  .shape({
-    email: yup.string().required("Campo obrigatório").email(),
-    password: yup
-      .string()
-      .required("Campo obrigatório")
-      .min(6, "Minímo de 8 caracteres"),
-  })
-  .required();
-
+const Schema = yup.object().shape({
+  email: yup.string().required("Campo obrigatório").email(),
+  password: yup
+    .string()
+    .required("Campo obrigatório")
+    .min(6, "Minímo de 6 caracteres"),
+});
 export default Schema;
